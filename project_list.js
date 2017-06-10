f3321fc290f01a9efa05916233cb332c61d73550
Leo Smith <p4p1@users.noreply.github.com>
2017-06-11 01:33:59 +0200

    :space_invader:

---
+++ project_list.js
@@ -0,0 +1,16 @@
+/*
+<table id="box_list" style="width:100%">
+    <caption><h3>Click the icon to go on a project:</h3></caption>
+    <tbody>
+        <tr>
+            <th><img width="15px" height="15px" src="img/peach_px.png" onclick="disp_p4p1();" /> | p4p1</th>
+            <th><img width="15px" height="15px" src="img/lemon_px.png" onclick="disp_larp();" /> | larp</th>
+            <th><img width="15px" height="15px" src="img/flopi.png" onclick="disp_kb();" /> | kb_arduino</th>
+        </tr>
+    </tbody>
+</table>
+    */
+
+function disp_list() {
+    document.getElementById('box_content').innerHTML = '<table id="box_list" style="width:100%"><caption><h3>Click the icon to go on a project:</h3></caption><tbody><tr><th><img width="15px" height="15px" src="img/peach_px.png" onclick="disp_p4p1();" /> | p4p1</th><th><img width="15px" height="15px" src="img/lemon_px.png" onclick="disp_larp();" /> | larp</th><th><img width="15px" height="15px" src="img/flopi.png" onclick="disp_kb();" /> | kb_arduino</th></tr></tbody></table>';
+}
--- projects.js
+++ projects.js
@@ -1,7 +1,4 @@
-/* FILE TO ADD PROJECTS */
 function disp_Projects() {
-
-    document.getElementById('box_content').innerHTML='<div id="box_header"><h2 id="inner_box_title"><span id="box_logo"></span> | <span id="box_title"></span></h2></div><div id="box_description"></div><table id="box_footer" style="width:100%"><tr><th>&lt;</th><th id="box_footer_left_icon"></th><th>|</th><th><a id="box_footer_source" href="#">source</a></th><th>|</th><th id="box_footer_right_icon"></th><th>&gt;</th></tr></table>';
 
     var i = Math.floor((Math.random() * 3));
 
@@ -17,6 +14,8 @@ function disp_Projects() {
 
 function disp_p4p1() {
 
+    document.getElementById('box_content').innerHTML='<div id="box_header"><h2 id="inner_box_title"><span id="box_logo"></span> | <span id="box_title"></span></h2></div><div id="box_description"></div><table id="box_footer" style="width:100%"><tr><th>&lt;</th><th id="box_footer_left_icon"></th><th>|</th><th><a id="box_footer_source" href="#">source</a></th><th>|</th><th id="box_footer_right_icon"></th><th>&gt;</th></tr></table>';
+
     document.getElementById('box_logo').innerHTML = '<img height="25px" width="25px" src="https://p4p1.github.io/img/peach_px.png" />';
     document.getElementById('box_title').innerHTML = 'p4p1';
     document.getElementById('box_description').innerHTML = '<p>p4p1, is a reverse shell for remote use of the windows dommand prompt. This reverse shell allows you to use the dos shell that is provided with cmd.exe as a remote tool.</p><img width="280px" height="156px" src="https://raw.githubusercontent.com/p4p1/p4p1/master/testing/Documentation/Screen%20Shot%202016-10-14%20at%2001.02.24.png" /><iframe width="280px" height="156px" src="https://www.youtube.com/embed/RpRrMuGo_y4" frameborder="0" allowfullscreen></iframe>';
@@ -28,6 +27,8 @@ function disp_p4p1() {
 
 function disp_larp() {
 
+    document.getElementById('box_content').innerHTML='<div id="box_header"><h2 id="inner_box_title"><span id="box_logo"></span> | <span id="box_title"></span></h2></div><div id="box_description"></div><table id="box_footer" style="width:100%"><tr><th>&lt;</th><th id="box_footer_left_icon"></th><th>|</th><th><a id="box_footer_source" href="#">source</a></th><th>|</th><th id="box_footer_right_icon"></th><th>&gt;</th></tr></table>';
+
     document.getElementById('box_logo').innerHTML = '<img height="25px" width="25px" src="https://p4p1.github.io/img/lemon_px.png" />';
     document.getElementById('box_title').innerHTML = 'larp';
     document.getElementById('box_description').innerHTML = '<p>larp, or leo arp is a tool to arp spoof you\'re entire network. I made this for educational purposes  so please do not use this for illegal activites. larp has different sniffers implemented to get data from the current network. you can also add you\'re own sniffers.</p><img src=""width="40%" height="40%" /><iframe width="40%" height="40%" src="" frameborder="0" allowfullscreen></iframe>';
@@ -38,6 +39,8 @@ function disp_larp() {
 }
 
 function disp_kb() {
+
+    document.getElementById('box_content').innerHTML='<div id="box_header"><h2 id="inner_box_title"><span id="box_logo"></span> | <span id="box_title"></span></h2></div><div id="box_description"></div><table id="box_footer" style="width:100%"><tr><th>&lt;</th><th id="box_footer_left_icon"></th><th>|</th><th><a id="box_footer_source" href="#">source</a></th><th>|</th><th id="box_footer_right_icon"></th><th>&gt;</th></tr></table>';
 
     document.getElementById('box_logo').innerHTML = '<img height="25px" width="25px" src="https://p4p1.github.io/img/flopi.png" />';
     document.getElementById('box_title').innerHTML = 'kb_arduino_lib';
