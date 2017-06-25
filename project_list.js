59efe04d971952119b06d471c2d37e88f27e7f2c
Berryllion <frambyboise@gmail.com>
2017-06-25 21:32:22 +0200

    Update project_list.js

--- project_list.js
+++ project_list.js
@@ -12,5 +12,5 @@
     */
 
 function disp_list() {
-    document.getElementById('box_content').innerHTML = '<table id="box_list" style="width:100%"><caption><h3>Click the icon to go on a project:</h3></caption><tbody><tr><th><img src="img/pixpeachx2.png" onclick="disp_p4p1();" /> | p4p1</th><th><img src="img/lemonx2.png" onclick="disp_larp();" /> | larp</th><th><img src="img/flopx2.png" onclick="disp_kb();" /> | kb_arduino</th></tr></tbody></table>';
+    document.getElementById('box_content').innerHTML = '<table id="box_list" style="width:100%"><caption><h3>Click the icon to go on a project:</h3></caption><tbody><tr><th><img src="img/peachx2.png" onclick="disp_p4p1();" /> | p4p1</th><th><img src="img/lemonx2.png" onclick="disp_larp();" /> | larp</th><th><img src="img/flopx2.png" onclick="disp_kb();" /> | kb_arduino</th></tr></tbody></table>';
 }
