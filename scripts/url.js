27202ca204c868061fab2328e2b4dc67cacc0553
Leo Smith <p4p1@users.noreply.github.com>
2017-08-09 21:18:56 +0200

    Add files via upload

--- scripts/url.js
+++ scripts/url.js
@@ -13,17 +13,6 @@ function $_GET(param) {
     vars;
 }
 
-function move_list(){
-    if (document.URL.indexOf("?") > 0) {
-        if (document.URL.indexOf("pos") > 0) {
-            document.location.href = document.URL.replace("pos=pres", "pos=list")
-        } else {
-            document.location.href = document.URL+"&pos=list";
-        }
-    } else {
-        document.location.href = document.URL+"?pos=list";
-    }
-}
 function change_lang(l) {
     if (document.URL.indexOf("?") > 0) {
         if (document.URL.indexOf("lang") > 0) {
@@ -39,29 +28,4 @@ function change_lang(l) {
         document.location.href = document.URL+"?lang="+l;
     }
 
-}
-function move_presentation(){
-    if (document.URL.indexOf("?") > 0) {
-        if (document.URL.indexOf("pos") > 0) {
-            document.location.href = document.URL.replace("pos=list", "pos=pres")
-        } else {
-            document.location.href = document.URL+"&pos=pres";
-        }
-    } else {
-        document.location.href = document.URL+"?pos=pres";
-    }
-}
-var lang="English";
-if ( $_GET("lang") == "fr" ) {
-    lang = "French";
-    document.getElementById('presentation_link').innerHTML = "Pr&eacute;sentation";
-    document.getElementById('project_list_link').innerHTML = "Projets";
-    document.getElementById('email_link').innerHTML = "Email";
-} else {
-    lang = "English";
-}
-if ($_GET("pos") == "list") {
-    disp_list(lang);
-} else {
-    disp_presentation(lang);
 }
