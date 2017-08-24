a8b006fb4178e123ba14fe3ec515ba90ed1918dd
p4p1 <k0oo@icloud.com>
2017-08-24 21:11:33 +0200

    improved resume

---
+++ scripts/img.js
@@ -0,0 +1,7 @@
+function change_img(current) {
+    if (current) {
+        document.getElementById('resume_img').src = "img/resume-2.jpg";
+    } else {
+        document.getElementById('resume_img').src = "img/resume-1.jpg";
+    }
+}
