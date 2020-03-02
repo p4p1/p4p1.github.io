4503339010e52d40117e3298bd30711b67ff2c17
papi <leo.smith@epitech.eu>
2020-03-02 14:47:34 +0100

    :space_invader:

--- desktop/scripts/img.js
+++
@@ -1,7 +0,0 @@
-function change_img(current) {
-    if (current) {
-        document.getElementById('resume_img').src = "img/resume-2.jpg";
-    } else {
-        document.getElementById('resume_img').src = "img/fr_resume-2.jpg";
-    }
-}
