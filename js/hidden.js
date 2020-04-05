7adcb3c19c5d764a2eddc505103b06075c8b0725
p4p1 <k0oo@icloud.com>
2020-04-05 20:23:32 +0200

    added shader icon

--- js/hidden.js
+++ js/hidden.js
@@ -90,3 +90,47 @@ function logKey(e) {
       }
     }
 }
+function shade() {
+
+  if (dark == 0) {
+    document.cookie = "dark=1";
+    document.body.style.backgroundColor = "#353333"
+    document.body.style.color = "#FFFFFF"
+    for(var i = 0; i < h1Elements.length; i++) {
+         h1Elements[i].style.color = "#fff";
+    }
+    for(var i = 0; i < h2Elements.length; i++) {
+         h2Elements[i].style.color = "#fff";
+    }
+    for(var i = 0; i < h3Elements.length; i++) {
+         h3Elements[i].style.color = "#fff";
+    }
+    for(var i = 0; i < h4Elements.length; i++) {
+         h4Elements[i].style.color = "#fff";
+    }
+    for(var i = 0; i < hrElements.length; i++) {
+         hrElements[i].style.borderTop = "1px solid rgba(255, 255, 255, 0.1)";
+    }
+    dark = 1;
+  } else {
+    document.cookie = "dark=0";
+    dark = 0;
+    document.body.style.backgroundColor = "#FFFFFF"
+    document.body.style.color = "#868e96"
+    for(var i = 0; i < h1Elements.length; i++) {
+         h1Elements[i].style.color = "#343a40";
+    }
+    for(var i = 0; i < h2Elements.length; i++) {
+         h2Elements[i].style.color = "#343a40";
+    }
+    for(var i = 0; i < h3Elements.length; i++) {
+         h3Elements[i].style.color = "#343a40";
+    }
+    for(var i = 0; i < h4Elements.length; i++) {
+         h4Elements[i].style.color = "#343a40";
+    }
+    for(var i = 0; i < hrElements.length; i++) {
+         hrElements[i].style.borderTop = "1px solid rgba(0,0,0,0.1)";
+    }
+  }
+}
